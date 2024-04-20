ddb0f8689b3351ae63df8ad7a8a74b227109ed8d
ggamja <dev.chan.8888@gmail.com>
2024-04-20 21:11:53 +0900

    test commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os, shutil
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+
+class ImageMoverApp:
+    def __init__(self) -> None:
+        pass
+    
+print('hi')
